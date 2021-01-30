1239577441af24b6e65da9a249b9bf3ab700d142
jjaen0823 <poungki0823@gmail.com>
2021-01-30 15:31:00 +0900

    14.03 modified order

--- order/forms.py
+++ order/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.db import transaction
+
 from .models import Order
+from product.models import Product
+from fcuser.models import Fcuser
 
 
 class RegisterForm(forms.Form):
-
     def __init__(self, request, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.request = request
@@ -28,4 +31,22 @@ class RegisterForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
-        product = cleaned_data.get('product')
+        product = cleaned_data.get('product')  # 해당 product 객체에 대한 pk 를 가지고 옴
+        fcuser = self.request.session.get(
+            'user')  # 해당 fcuser 객체에 대한 pk 를 가지고 옴
+
+        if quantity and quantity > 0 and product and fcuser:
+            with transaction.atomic():
+                prod = Product.objects.get(pk=product)
+                order = Order(
+                    quantity=quantity,
+                    product=prod,
+                    fcuser=Fcuser.objects.get(email=fcuser)
+                )
+                order.save()
+                prod.stock -= quantity
+                prod.save()
+        else:
+            self.product = product
+            self.add_error('quantity', 'There is no quantity.')
+            self.add_error('product', 'There is no product.')
--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 
 from .forms import RegisterForm
@@ -10,3 +10,16 @@ from .forms import RegisterForm
 class OrderCreate(FormView):
     form_class = RegisterForm
     success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.product))
+
+    # FormView 안에서도 request를 전달할 수 있도록 만들어줘야 함
+
+    def get_form_kwargs(self, **kwargs):
+        # form을 생성할 때 어떤 인자값을 전달해서 만들 것인지 결정하는 함수
+        kw = super().get_form_kwargs(**kwargs)
+        kw.update({
+            'request': self.request
+        })
+        return kw
